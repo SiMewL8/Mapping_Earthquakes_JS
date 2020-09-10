4f7766bccc5273d10878606047532647989c11f4
SiMewL8 <64818157+SiMewL8@users.noreply.github.com>
2020-09-09 19:37:47 -0700

    added circle markers

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -7,17 +7,41 @@ console.log("working");
 
 // let testmap = L.map('leafmap').setview([40.7,-94.5],4);
 
-// alternate way to create map box
+// alternate way to create map box: zoom in LA
 let testmap = L.map("leafmap", {
     center: [
-        40.7, -94.5
+        34.0522, -118.2437
     ],
-    zoom: 4
+    zoom: 14
 });
+
+
+
+// single points mapping: los angeles
+// var marker = L.marker([34.0522, -118.2437]).addTo(testmap);
+
+
+// circle: LA
+// var laCircle = L.circle(
+//     [34.0522, -118.2437],
+//     {radius: 100}).addTo(testmap);
+
+// skill circle: LA
+// create a light-yellow circle with black lines indicating a 300-meter radius of
+// Central Los Angeles on a dark map
+var darkLACircle = L.circleMarker(
+    [34.0522, -118.2437], {
+        color: 'black',
+        fillColor: '#fee8c8', //light-yellow hex
+        radius:300
+    }
+).addTo(testmap);
+
+
 
 // tile layer method: street level edition
 // create tile layer that will be the map background
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', { //map box styles api: street level tiles
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', { //map box styles api: street level tiles
 
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     
